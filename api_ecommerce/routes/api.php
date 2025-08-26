9ba435a341af54854557ab1b291ac665fef6d8cc
Mateo Rojas <mateorojasarce2003@gmail.com>
2025-08-25 22:12:27 -0300

    primercommit

---
+++ api_ecommerce/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+ 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+ 
+Route::group([
+ 
+   // 'middleware' => 'api',
+    'prefix' => 'auth'
+ 
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/verified_auth', [AuthController::class, 'verified_auth'])->name('verified_auth');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login_ecommerce', [AuthController::class, 'login_ecommerce'])->name('login_ecommerce');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('/me', [AuthController::class, 'me'])->name('me');
+});
